453c44f435fa00ba2edd325851d844f7c2668024
Rumeet Goradia <rumeet.goradia@gmail.com>
2019-04-25 21:02:17 -0400

    Clean clientfunctions.h

--- clientfunctions.h
+++ clientfunctions.h
@@ -1,7 +1,2 @@
-typedef struct node {
-	char *token;
-	struct node *next;
-} node;
-
 int add(int fd_manifest, char *hashcode, char *path, char *input);
 int remover(int fd_manifest, char *path, char *input);
