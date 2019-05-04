836b86664aebee1402cf892cd5fb816693cb7905
Rumeet U. Goradia <rumeet.goradia@gmail.com>
2019-05-04 16:07:37 -0400

    Continuing commit

---
+++ WTFserver.c
@@ -0,0 +1,403 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/sendfile.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <signal.h>
+#include <errno.h>
+#include <time.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include "helperfunctions.h"
+
+struct server_context {
+	unsigned int num_connections;
+	pthread_mutex_t lock;
+};
+
+struct work_args {
+	int socket;
+	struct server_context *cntx;
+};
+
+void *handler (void *args);
+
+int main (int argc, char **argv) {
+	/* Check for correct number of arguments */
+	if (argc < 2) {
+		fprintf(stderr, "ERROR: Need port number.\n");
+		return(EXIT_FAILURE);
+	} else if (argc > 2) {
+		fprintf(stderr, "ERROR: Too many arguments.\n");
+		return(EXIT_FAILURE);
+	}
+
+	struct server_context *cntx = (struct server_context *) malloc(sizeof(struct server_context));
+	cntx->num_connections = 0;
+	pthread_mutex_init(&cntx->lock, NULL);
+
+	sigset_t sig;
+	sigemptyset(&sig);
+	sigaddset(&sig, SIGPIPE);
+	if (pthread_sigmask(SIG_BLOCK, &sig, NULL) != 0) {
+		fprintf(stderr, "ERROR: Unable to mask SIGPIPE.\n");
+		return(EXIT_FAILURE);
+	}
+	
+	/* Get port number */
+	int port = atoi(argv[1]);
+	if (port < 1024) {
+		fprintf(stderr, "ERROR: Invalid port number.\n");
+		return EXIT_FAILURE;
+	}
+	
+	int server_socket, client_socket;
+	pthread_t thread;
+	struct addrinfo hints, *res, *p;
+	struct sockaddr_storage *client_add;
+	int option = 1;
+	socklen_t sin_size = sizeof(struct sockaddr_storage);
+	struct work_args *wa;
+
+	/* Initialize hints for getaddrinfo */
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	if (getaddrinfo(NULL, argv[1], &hints, &res) != 0) {
+		fprintf(stderr, "ERROR: getaddrinfo() failed.\n");
+		pthread_exit(NULL);
+	}
+	
+	for (p = res; p != NULL; p = p->ai_next) {
+		/* Create socket */
+		server_socket = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+		if (server_socket < 0) {
+			fprintf(stderr, "ERROR: Could not open socket.\n");
+			continue;
+		}
+
+		/* Make address resuable */
+		if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(int)) < 0) {
+			fprintf(stderr, "ERROR: Socket setsockopt() failed.\n");
+			close(server_socket);
+			continue;
+		}
+
+		/* Bind socket to address*/
+		if (bind(server_socket, p->ai_addr, p->ai_addrlen) < 0) {
+			fprintf(stderr, "ERROR: Socket bind() failed.\n");
+			close(server_socket);
+			continue;
+		}
+
+		/* Listen */
+		if (listen(server_socket, 20) < 0) {
+			fprintf(stderr, "ERROR: Socket listen() failed.\n");
+			close(server_socket);
+			continue;
+		}
+		
+		break;
+	}
+	freeaddrinfo(res);
+	if (p == NULL) {
+		fprintf(stderr, "ERROR: Could not bind to any socket.\n");
+		pthread_exit(NULL);
+	}
+	
+	/* Create server directory if doesn't already exist */
+	struct stat st = {0};
+	if (stat("./.server_directory", &st) == -1) {
+		mkdir("./.server_directory", 0744);
+	}
+
+	printf("Waiting for client...\n");
+	while(1) {
+		client_add = malloc(sin_size);
+		/* Accept */
+		if ((client_socket = accept(server_socket, (struct sockaddr *) &client_add, &sin_size)) < 0) {
+			fprintf(stderr, "ERROR: Could not accept connection.\n");
+			free(client_add);
+			continue;
+		}
+		
+		wa = malloc(sizeof(struct work_args));
+		wa->socket = client_socket;
+		wa->cntx = cntx;
+
+		if(pthread_create(&thread, NULL, handler, wa) != 0) {
+			fprintf(stderr, "ERROR: Could not create thread.\n");
+			free(client_add);
+			free(wa);
+			close(client_socket);
+			close(server_socket);
+			return EXIT_FAILURE;
+		}
+	}
+	
+	pthread_mutex_destroy(&cntx->lock);
+	free(cntx);
+	return EXIT_SUCCESS;
+}
+
+void *handler(void *args) {
+	struct work_args *wa;
+	struct server_context *cntx;
+	int client_socket, sent, received, i;	
+	char recv_buffer[BUFSIZ];
+
+	wa = (struct work_args *) args;
+	client_socket = wa->socket;
+	cntx = wa->cntx;
+
+	pthread_detach(pthread_self());
+
+	printf("Socket %d connected.\n", client_socket);
+	
+	received = recv(client_socket, recv_buffer, BUFSIZ - 1, 0);
+	recv_buffer[received] = '\0';
+	if (received <= 0) {
+		fprintf(stderr, "ERROR: Server-side recv() failed.\n");
+		pthread_exit(NULL);
+	}
+	char *token;
+	token = strtok(recv_buffer, ":");
+
+	if (token[0] == 'c') {
+
+		token = strtok(NULL, ":");
+
+		char *new_proj_path = (char *) malloc(strlen(token) + 22);
+		if (token[strlen(token) - 1] != '/') {
+			snprintf(new_proj_path, strlen(token) + 22, "./.server_directory/%s/", token);
+		} else {
+			snprintf(new_proj_path, strlen(token) + 22, "./.server_directory/%s", token);
+		}
+		char sending[2];
+/*		struct stat st = {0};
+		if (stat(new_proj_path, &st) == -1) { */
+		if (check_dir(new_proj_path) == -1) {
+			mkdir(new_proj_path, 0744);
+			char *new_mani_path = (char *) malloc(strlen(new_proj_path) + 11);
+			snprintf(new_mani_path, strlen(new_proj_path) + 11, "%s.Manifest", new_proj_path);
+			int fd_mani = open(new_mani_path, O_CREAT | O_WRONLY, 0744);
+			write(fd_mani, "0\n", 2);
+			close(fd_mani);
+			free(new_mani_path);
+			free(new_proj_path);
+			sending[0] = 'c';
+			sent = send(client_socket, sending, 2, 0);
+			printf("Creation of project \"%s\" successful.\n", token);
+			pthread_exit(NULL);
+		} else {
+			sending[0] = 'x';
+			sent = send(client_socket, sending, 2, 0);
+			free(new_proj_path);
+			fprintf(stderr, "ERROR: Creation of project \"%s\" failed.\n", token);
+			pthread_exit(NULL);
+		}
+	} else if (token[0] == 'd') {
+		token = strtok(NULL, ":");
+		char *proj_path = (char *) malloc(strlen(token) + 22);
+		snprintf(proj_path, strlen(token) + 22, ".server_directory/%s", token);
+		char sending[2];
+		if (check_dir(proj_path) == -1) {
+			fprintf(stderr, "ERROR: Project \"%s\" does not exist on server.\n", token);
+			sending[0] = 'x';
+			sent = send(client_socket, sending, 2, 0);
+			free(proj_path);
+			pthread_exit(NULL);	
+		} else {
+			int check = remove_dir(proj_path);	
+			if (check == 0) {
+				sending[0] = 'g';
+				sent = send(client_socket, sending, 2, 0);
+				free(proj_path);
+				pthread_exit(NULL);
+			} else {
+				sending[0] = 'b';
+				sent = send(client_socket, sending, 2, 0);
+				fprintf(stderr, "ERROR: Could not remove \"%s\" project from server.\n", token);
+				free(proj_path);
+				pthread_exit(NULL);
+			}
+		}
+	} else if (token[0] == 'v') {
+		token = strtok(NULL, ":");
+		char *proj_path = (char *) malloc(strlen(token) + 22);
+		snprintf(proj_path, strlen(token) + 22, ".server_directory/%s", token);
+		if (check_dir(proj_path) == -1) {
+			char sending[2] = "b";
+			sent = send(client_socket, sending, 2, 0);
+			fprintf(stderr, "ERROR: Project \"%s\" does not exist on server.\n", token);
+			free(proj_path);
+			pthread_exit(NULL);
+		}
+		free(proj_path);
+		char *mani_path = (char *) malloc(strlen(token) + 31);
+		snprintf(mani_path, strlen(token) + 31, ".server_directory/%s/.Manifest", token);
+		int fd_mani = open(mani_path, O_RDONLY);
+		if (fd_mani < 0) {
+			fprintf(stderr, "ERROR: Unable to open \".Manifest\" file for \"%s\" project.\n", token);
+			free(mani_path);
+			free(proj_path);
+			char sending[2] = "x";
+			sent = send(client_socket, sending, 2, 0);
+			pthread_exit(NULL);	
+		}
+		struct stat st = {0};
+		if (fstat(fd_mani, &st) < 0) {
+			fprintf(stderr, "ERROR: fstat() failed.\n");
+			char sending[2] = "x";
+			sent = send(client_socket, sending, 2, 0);
+			free(mani_path);
+
+			pthread_exit(NULL);
+
+		}
+		char file_size[256];
+		snprintf(file_size, 256, "%d", st.st_size);
+		sent = send(client_socket, file_size, 256, 0);	
+		if (sent < 0) {
+			fprintf(stderr, "ERROR: Could not send size of \"%s\".\n", mani_path);
+			free(mani_path);
+			char sending[2] = "x";
+			sent = send(client_socket, sending, 2, 0);
+			pthread_exit(NULL);	
+		}
+		char contents[st.st_size + 1];
+		int bytes_read = read(fd_mani, contents, st.st_size);
+		contents[bytes_read] = '\0';
+		sent = send(client_socket, contents, bytes_read, 0);
+		/* off_t offset = 0;
+		int remaining = st.st_size;
+		while (((sent = sendclient_(socket, (contents + offset), st.st_size, 0)) > 0) && (remaining > 0)) {
+			printf("sending: %s", contents+offset);
+			remaining -= sent;
+			offset += sent;
+		} */
+		printf("Sent \".Manifest\" file for \"%s\" project to client.\n", token);
+		free(mani_path);
+		pthread_exit(NULL);
+	} else if (token[0] == 'o') {
+		token = strtok(NULL, ":");
+		char *proj_path = (char *) malloc(strlen(token) + 22);
+		snprintf(proj_path, strlen(token) + 22, ".server_directory/%s", token);
+		if (check_dir(proj_path) == -1) {
+			char sending[2] = "b";
+			sent = send(client_socket, sending, 2, 0);
+			free(proj_path);
+			fprintf(stderr, "ERROR: Project \"%s\" does not exist on server.\n", token);
+			pthread_exit(NULL);
+		}
+		free(proj_path);
+		char *mani_path = (char *) malloc(strlen(token) + 31);
+		snprintf(mani_path, strlen(token) + 31, ".server_directory/%s/.Manifest", token);
+		int fd_mani = open(mani_path, O_RDONLY);
+		if (fd_mani < 0) {
+			fprintf(stderr, "ERROR: Unable to open \".Manifest\" file for \"%s\" project.\n", token);
+			free(mani_path);
+			free(proj_path);
+			char sending[2] = "x";
+			sent = send(client_socket, sending, 2, 0);
+			pthread_exit(NULL);	
+		}
+		struct stat st = {0};
+		if (fstat(fd_mani, &st) < 0) {
+			fprintf(stderr, "ERROR: fstat() failed.\n");
+			char sending[2] = "x";
+			sent = send(client_socket, sending, 2, 0);
+			free(mani_path);
+			pthread_exit(NULL);
+
+		}
+		char file_size[256];
+		snprintf(file_size, 256, "%d", st.st_size);
+		sent = send(client_socket, file_size, 256, 0);	
+		if (sent < 0) {
+			fprintf(stderr, "ERROR: Could not send size of \"%s\".\n", mani_path);
+			free(mani_path);
+			char sending[2] = "x";
+			sent = send(client_socket, sending, 2, 0);
+			pthread_exit(NULL);	
+		}
+		char contents[st.st_size + 1];
+		int bytes_read = read(fd_mani, contents, st.st_size);
+		contents[bytes_read] = '\0';
+		sent = send(client_socket, contents, bytes_read, 0);
+		char *buffer = (char *) malloc(BUFSIZ);;
+		recv(client_socket, buffer, BUFSIZ, 0);
+		int remaining = atoi(buffer);
+		printf("remaining: %d\n", remaining);
+		recv(client_socket, buffer, BUFSIZ, 0);
+		int version = atoi(buffer);
+		printf("version: %d\n", version);
+		free(buffer);
+		char *comm_path = (char *) malloc(strlen(token) + 28 + sizeof(version));
+		snprintf(comm_path, strlen(token) + 28 + sizeof(version), ".server_directory/%s/.Commit%d", token, version);
+		int fd_comm_server = open(comm_path, O_CREAT | O_RDWR | O_APPEND, 0744);
+		if (fd_comm_server < 0) {
+			fprintf(stderr, "ERROR: Unable to create \".Commit%d\" file for \"%s\" project.\n", version, token);
+			free(comm_path);
+			char sending[2] = "d";
+			sent = send(client_socket, sending, 2, 0);
+			pthread_exit(NULL);
+		}
+		free(comm_path);
+		char comm_buff[remaining + 1];
+		size_t total = 0;
+		while (total < remaining) {
+			ssize_t nb = recv(client_socket, comm_buff, remaining - total, 0);
+			total += nb;
+			if (nb == -1) {
+				fprintf(stderr, "ERROR: Server recv() failed.\n");
+				break;
+			}
+			if (write(fd_comm_server, comm_buff, nb) == -1) {
+				fprintf(stderr, "Write to \".Commit%d\" failed.\n", version);
+			}
+		}
+		printf("total: %d\n", total);
+
+	/*	int len = 0;
+		printf("passed creation\n");
+		while ((remaining > 0) && ((len = recv(client_socket, buffer, BUFSIZ, 0)) > 0)) {
+			printf("%s\n", buffer);
+			write(fd_comm_server, buffer, len);
+			remaining -= len;
+		} */
+		char sending[2] = "g";
+		sent = send(client_socket, sending, 2, 0);
+		close(fd_comm_server);
+	}
+	
+
+/*	while(1) {
+		sprintf(sending, "Hello socket! It is %d.\n", (int) time(NULL));
+
+		sent = send(socket, sending, strlen(sending), 0);
+		if (sent < 0 && (errno == EPIPE || errno == ECONNRESET)) {
+			printf("Socket %d disconnected.\n", socket);
+			close(socket);
+			free(wa);
+			pthread_exit(NULL);
+		} else if (sent < 0) {
+			fprintf(stderr, "ERROR: Something went wrong while sending.\n");
+			free(wa);
+			pthread_exit(NULL);
+		}
+		sleep(5);
+	}
+*/	
+	pthread_exit(NULL);
+}
+
---
+++ helperfunctions.c
@@ -0,0 +1,253 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <math.h>
+#include <openssl/sha.h>
+
+
+unsigned int tokenize(char *path, char *input, char *hash) {
+	if (input == NULL) {
+		return 0;	
+	}
+	char whole_string[strlen(input)];
+	strcpy(whole_string, input);
+	char *token;
+	unsigned int byte_count = 0;
+	unsigned int prev_bytes = 0;
+	short check = 0;
+	token = strtok(whole_string, "\n\t");
+	prev_bytes = strlen(token) + 1;
+	byte_count += prev_bytes;
+	while (token != NULL) {
+		token = strtok(NULL, "\n\t");
+		if (token == NULL) {
+			break;
+		}
+		if (check == 1) {
+			if (strcmp(token, hash) == 0) {
+					return -1;
+			} else {
+				return byte_count;
+			}
+		}
+		if (strcmp(token, path) == 0) {
+			check = 1;
+		}
+		prev_bytes = strlen(token) + 1;
+		byte_count += prev_bytes;
+	}
+	return strlen(input);
+	
+}
+
+int add(int fd_manifest, char *hashcode, char *path, char *input) {
+	int *version = (int *) malloc(sizeof(int));
+	int move = tokenize(path, input, hashcode);
+	if (move == 0) {
+		fprintf(stderr, "ERROR: Could not read \".Manifest\" file.\n");
+		return -1;
+	}
+	if (move == -1) {
+		fprintf(stderr, "ERROR: File already up-to-date in local \".Manifest\" file.\n");
+		return -1;
+	}
+	if (move == -2) {
+		return -2;
+	}
+/*	char buff[move];
+	read(fd_manifest, buff, move); */
+	lseek(fd_manifest, move, SEEK_SET);
+	if (move == strlen(input)) {
+		write(fd_manifest, "0\t", 2);
+		write(fd_manifest, path, strlen(path));
+		write(fd_manifest, "\t", 1);
+		write(fd_manifest, hashcode, strlen(hashcode));	
+		write(fd_manifest, "\n", 1);
+	} else {
+		write(fd_manifest, hashcode, strlen(hashcode));
+	}
+	return 0;
+}
+
+int remover(int fd_manifest, char *path, char *input) {
+	char dashes[65] = "----------------------------------------------------------------";
+	int move = tokenize(path, input, dashes);
+	if (move == strlen(input)) {
+		fprintf(stderr, "ERROR: File \"%s\" not in \".Manifest\" file.\n", path);
+		return -1;
+	}
+	if (move == -2) {
+		fprintf(stderr, "ERROR: File \"%s\" already removed from \".Manifest\" file.\n", path);
+		return -1;
+	}
+/*	char buff[move];
+	read(fd_manifest, buff, move); */
+	lseek(fd_manifest, move, SEEK_SET);
+	write(fd_manifest, dashes, strlen(dashes));
+	return 0;
+}
+
+int check_dir(char *path) {
+	DIR *dr = opendir(path);
+	if (dr == NULL) {
+		return -1;	
+	}
+	return 0;
+}
+
+int remove_dir(char *path) {
+	DIR *dr;
+	size_t path_len = strlen(path);
+	int ret = -1;
+	if (!(dr = opendir(path))) {
+		fprintf(stderr, "ERROR: Could not open project \"%s\" on server's side.\n", (path + 17));
+		return ret;
+	}
+	struct dirent *de;
+	ret = 0;
+	while ((de = readdir(dr)) != NULL) {
+		if (strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0) {
+			continue;
+		}
+		char *new_path = (char *) malloc(strlen(de->d_name) + strlen(path) + 2);
+		if (new_path != NULL) {
+			snprintf(new_path, strlen(de->d_name) + strlen(path) + 2, "%s/%s", path, de->d_name);
+			if (de->d_type == DT_DIR) {
+				ret = remove_dir(new_path);
+			} else {
+				ret = remove(new_path);
+			}
+			if (ret < 0) {
+				fprintf(stderr, "ERROR: Failed to delete \"%s\" from server.", new_path);
+				return ret;
+			}
+		}
+		free(new_path);
+	}
+	ret = rmdir(path);
+	if (ret < 0) {
+		fprintf(stderr, "ERROR: Failed to delete \"%s\" from server.", path);
+	}
+	return ret;
+}
+
+int get_file_size(int fd) {
+	struct stat st = {0};
+	if (fstat(fd, &st) < 0) {
+		fprintf(stderr, "ERROR: fstat() failed.\n");
+		return -1;
+	}
+	return st.st_size;
+}
+
+int commit_check(char *path, char *hash, int given_version, char *other_mani, int dashes) {
+	char *token = strtok(other_mani, "\n");
+	int count = 0;
+	int check = 0;
+	int version = 0;
+	while (token != NULL) {
+		token = strtok(NULL, "\n\t");
+		if (token == NULL) {
+			break;
+		}
+		++count;
+		if (check == 1) {
+			if (strcmp(token, hash) != 0 && version >= given_version) {
+				return -1;
+			} else if ((strcmp(token, hash) != 0 && !dashes) || (strcmp(token, "----------------------------------------------------------------") != 0 && dashes)) {
+				return 2;
+			}
+			break;
+		}
+		if (count % 3 == 1) {
+			version = atoi(token);
+		} else if (count % 3 == 2) {
+			if (strcmp(token, path) == 0) {
+				check = 1;
+			}
+		}
+			
+	}
+	return 0;
+}
+
+int commit(int fd_comm, char *client_mani, char *server_mani, int fd_mani) {
+
+	char temp[strlen(client_mani) + 1];
+	strcpy(temp, client_mani);
+	char *token = strtok(temp, "\n");
+	int count = 0;
+	int version = 0;
+	unsigned char hash[SHA256_DIGEST_LENGTH];
+	char hashed[SHA256_DIGEST_LENGTH * 2 + 1];
+	char *path = NULL;
+	int bytes = 2;
+	while (token != NULL) {
+		token = strtok(NULL, "\t\n");
+		++count;
+		if (token == NULL) {
+			if (bytes < strlen(client_mani) - 1) {
+				strcpy(temp, client_mani);
+				token = strtok(&(temp[bytes - 1]), "\t\n"); 
+			} else {
+				break;
+			}
+		}
+
+		if (count % 3 == 1) {
+			version = atoi(token);
+			++version;
+		} else if (count % 3 == 2) {
+			int fd = open(token, O_RDONLY);
+			int size = get_file_size(fd);
+			char buff[size + 1];
+			read(fd, buff, size);
+			SHA256(buff, strlen(buff), hash);
+			int i = 0;
+			for (i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
+				sprintf(hashed + (i * 2), "%02x", hash[i]);
+			}
+			path = malloc(strlen(token) + 1);
+			snprintf(path, strlen(token) + 1, "%s", token);
+		} else {
+
+			int check = strcmp(token, hashed);
+			int dash_check = strcmp(token, "----------------------------------------------------------------");
+			int comm_check = commit_check(path, hash, version, server_mani, dash_check);
+
+			if (dash_check != 0) {
+				if (comm_check == 2) {
+					write(fd_comm, "U\t", 2);
+				} else if (comm_check == 0) {
+					write(fd_comm, "A\t", 2);
+				}
+			}
+			if ((check != 0 || dash_check == 0) && comm_check == -1) {
+				return -1;
+			}
+			if ((check == 0 && comm_check == -1) || (dash_check == 0 && comm_check == 0)) {
+				continue;
+			}
+			if (dash_check == 0 && comm_check == 2) {
+				write(fd_comm, "D\t", 2);
+			}
+			char *version_string = (char *) malloc(sizeof(version) + 1);
+			snprintf(version_string, sizeof(version) + 1, "%d", version);
+			write(fd_comm, version_string, strlen(version_string));
+			write(fd_comm, "\t", 1);
+			write(fd_comm, path, strlen(path));
+			write(fd_comm, "\t", 1);
+			write(fd_comm, hashed, strlen(hashed));
+			write(fd_comm, "\n", 1);
+			free(version_string);
+		}
+		bytes += strlen(token) + 1;
+	}
+	return 1;
+		
+}
